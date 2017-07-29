e96a2602974afbc2fd23c4d9e641d86f82cdfe16
KamilMosakowski <kam.mosak@gmail.com>
2017-07-29 13:44:40 +0200

    running loras

--- SLIB/world1-1.lua
+++ SLIB/world1-1.lua
@@ -3,13 +3,74 @@ local composer = require( "composer" )
 
 local scene = composer.newScene()
 
+local physics = require( "physics" )
+physics.start()
+physics.setGravity( 0, 0 )
 -- -----------------------------------------------------------------------------------
 -- Code outside of the scene event functions below will only be executed ONCE unless
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
 -- -----------------------------------------------------------------------------------
 
+local lorasSheetOptions = {
+    frames = {
+        {   -- 1. loras run 1
+            x = 70,
+            y = 50,
+            width = 70,
+            height = 100,
+			sourceX = 0,
+            sourceY = 0,
+            sourceWidth = 0,
+            sourceHeight = 0
+        },
+		{   -- 2. loras run 2
+            x = 155,
+            y = 50,
+            width = 70,
+            height = 100,
+			sourceX = 0,
+            sourceY = 0,
+            sourceWidth = 0,
+            sourceHeight = 0
+        },
+		{   -- 3. loras run 3
+            x = 240,
+            y = 50,
+            width = 70,
+            height = 100,
+			sourceX = 0,
+            sourceY = 0,
+            sourceWidth = 0,
+            sourceHeight = 0
+        },
+		{   -- 4. loras run 4
+            x = 325,
+            y = 50,
+            width = 70,
+            height = 100,
+			sourceX = 0,
+            sourceY = 0,
+            sourceWidth = 0,
+            sourceHeight = 0
+        }
+    }
+}
 
+local lorasSheet = graphics.newImageSheet( "images/loras.png", lorasSheetOptions )
 
+-- Configure image sequences
+local lorasSequences = {
+    -- non-consecutive frames sequence
+    {
+        name = "run",
+        frames = { 1,2,3,4 },
+        time = 1000,
+        loopCount = 0,
+        loopDirection = "forward"
+    }
+}
+
+local loras
 
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
@@ -21,6 +82,31 @@ function scene:create( event )
 	local sceneGroup = self.view
 	-- Code here runs when the scene is first created but has not yet appeared on screen
 
+	physics.pause()  -- Temporarily pause the physics engine
+
+	-- Set up display groups
+	backGroup = display.newGroup()  -- Display group for the background image
+	sceneGroup:insert( backGroup )  -- Insert into the scene's view group
+
+	mainGroup = display.newGroup()  -- Display group for the ship, asteroids, lasers, etc.
+	sceneGroup:insert( mainGroup )  -- Insert into the scene's view group
+
+	uiGroup = display.newGroup()    -- Display group for UI objects like the score
+	sceneGroup:insert( uiGroup )    -- Insert into the scene's view group
+	
+	-- Load the background
+	--local background = display.newImageRect( backGroup, "game_bg.png", 800, 1400 )
+	--background.x = display.contentCenterX
+	--background.y = display.contentCenterY
+	--background.width = display.actualContentWidth
+    --background.height = display.actualContentHeight
+
+	loras = display.newSprite(mainGroup, lorasSheet, lorasSequences)
+	loras.x = 640--250
+	loras.y = 360
+
+	loras:setSequence( "run" )  -- switch to "idle" sequence
+    loras:play()  -- play the new sequence
 end
 
 
